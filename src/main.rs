6a2f934edfdb85dd21b8459b4cee7f5f4a473b7b
Abhishek Sunil <abhisheksunil92@gmail.com>
2024-04-04 11:02:48 +0530

    add decode functions for different encoded types

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod parse;
+use parse::decode_bencoded_value;
+// use serde_json;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+
+    if command == "decode" {
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
+    } else {
+        println!("unknown command: {}", args[1])
+    }
+}
